ab49b40d749e8d70fc573e88799aa7a0f445468c
adisoim <adisoim2002@gmail.com>
2022-07-14 17:40:37 +0300

    adaugare comentarii pe o singura linie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    //aceasta este metoda main care porneste o apl java
     public static void main(String[] args) {
+        //aici vrem sa afisam pe consola intellij un mesaj ales de noi
         System.out.println("Invat limbajul java si despre masina virtuala java");
 
     }
